bd7972541b209ec73e8b7921a66e300488b76d86
Chenwi <chenwieugene.j@gmail.com>
2021-09-21 08:50:11 +0100

    changed footer

--- script.js
+++ script.js
@@ -1,64 +1,37 @@
-/* function seeMore(){
-const moreProjects = document.getElementById('see-moreBtn')
-const portfolio = document.getElementById('portfolio-sc')
-const morePortfolio = document.getElementById('more-portfolio')
+function seeMore() {
+  const moreProjects = document.getElementById("see-moreBtn");
+  const portfolio = document.getElementById("portfolio-sc");
+  const morePortfolio = document.getElementById("more-portfolio");
+  const firstPortfolio = document.getElementById("portfolio-first");
 
-moreProjects.addEventListener('click', () => {
-    let value = morePortfolio.classList.contains('more-portfolio')
-    if(value){
-       morePortfolio.classList.remove('more-portfolio')
-       moreProjects.innerText = 'See less'
+  portfolio.innerHTML = firstPortfolio.innerHTML;
+
+  moreProjects.addEventListener("click", () => {
+    if (portfolio.innerHTML === firstPortfolio.innerHTML) {
+      portfolio.innerHTML = morePortfolio.innerHTML;
+      moreProjects.innerText = "<-- See less";
+    } else {
+      portfolio.innerHTML = firstPortfolio.innerHTML;
+      moreProjects.innerText = "See more -->";
     }
-   
-    else if(!(value)){
-        morePortfolio.classList.add('more-portfolio')
-        moreProjects.innerText = 'See more'
-    }
-} )
-    
-}    
+  });
+}
 
+const chat = document.querySelector(".bot");
+const chatIcon = document.querySelector(".fa-comment-dots");
+const closeChat = document.querySelector(".ifr__header");
 
-seeMore() */
+const displayChat = () => {
+  if (chat.classList.contains("slide")) {
+    chat.classList.remove("slide");
+    chatIcon.classList.add("slide");
+  } else {
+    chat.classList.add("slide");
+    chatIcon.classList.remove("slide");
+  }
+};
 
+chatIcon.addEventListener("click", () => displayChat());
+closeChat.addEventListener("click", () => displayChat());
 
- function seeMore(){
-const moreProjects = document.getElementById('see-moreBtn')
-const portfolio = document.getElementById('portfolio-sc')
-const morePortfolio = document.getElementById('more-portfolio')
-const firstPortfolio = document.getElementById('portfolio-first')
-
-portfolio.innerHTML = firstPortfolio.innerHTML;
-
-moreProjects.addEventListener('click', () => {
-    
-    if(portfolio.innerHTML === firstPortfolio.innerHTML){
-        
-       portfolio.innerHTML = morePortfolio.innerHTML;
-       moreProjects.innerText = '<-- See less';
-    }
-   
-    else{
-        portfolio.innerHTML = firstPortfolio.innerHTML;
-        moreProjects.innerText = 'See more -->';
-    }
-} )
-    
-}    
-
-
-seeMore()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+seeMore();
